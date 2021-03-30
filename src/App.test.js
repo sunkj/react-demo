3449841317a0ce51c67e3e49487397bf494b217a
sunkj <sunkanjue@163.com>
2021-03-30 18:36:02 +0800

    add travis

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders CI/CD react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/CI\/CD/i);
+  expect(linkElement).toBeInTheDocument();
+});
